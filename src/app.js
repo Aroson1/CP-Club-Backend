39cc766bab234d54cdb35771b418ccb0f9c6ab29
Aroson1 <74296409+Aroson1@users.noreply.github.com>
2024-08-26 19:04:58 +0530

    Changes cors policies

--- src/app.js
+++ src/app.js
@@ -29,12 +29,12 @@ app.disable("x-powered-by");
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:3001", 
-    credentials: true, 
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3001", 
+//     credentials: true, 
+//   })
+// );
 app.options("*", cors());
 
 app.use(
